de868c6baaf5b13fedea1138a3105d84b47eaadb
HarikrishnaMogan <harikrishss437@gmail.com>
2021-09-06 18:36:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props){
+        console.log("constructor");
+        super(props);
+        this.state={
+            count:0,
+        }
+    }
+
+    
+  componentDidUpdate(prevprops,prevstate)
+  {
+      console.log("component Updated");
+      console.log(prevstate.count,this.state.count);
+  }
+
+
+  componentWillUnmount()
+  {
+      console.log("Component Unmounted");
+  }
+
+
+    componentDidMount()
+    {
+        console.log("component mounted");
+    }
+
+
+    count = (v)=>{
+       if(v==="inc")
+       {
+           this.setState({count:this.state.count+1})
+       }
+       else if(v==="dec"){
+
+           this.setState({count:this.state.count-1})
+       }
+       else{
+           this.setState({count:0});
+       }
+    }
+
+
+
+    render()
+    {
+        console.log("render");
+        return(
+            <React.Fragment>
+             <p>{this.state.count}</p>
+             <button onClick={()=>{this.count("inc")}}>Incr</button>
+             <button onClick={()=>{this.count("dec")}}>Dec</button>
+             <button onClick={this.count}>Res</button>
+             <button onClick={()=>this.forceUpdate()}>Force</button>
+            </React.Fragment>
+            
+        )
+    }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Test extends React.Component{
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      value:true,
+    }
+  }
+  render()
+  {
+    return(
+      <>
+      <button onClick={()=>{this.setState({value:!this.state.value})}}>Hide/Show</button>
+     {this.state.value ?<App />:<></> }
+      </>
+    )
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Test />,document.querySelector("#root"));
